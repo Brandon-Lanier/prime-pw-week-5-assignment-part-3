baa201510de8cea03379d7fd5a5cd8c0fe752932
Brandon Lanier <brandonjlanier@gmail.com>
2022-01-10 12:16:56 -0600

    Added the function that will accept the input parameters of title, artist, and yearpublished

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let record = {
+    title,
+    artist,
+    yearPublished
+  } // end of object
+  collection.push(record);
+  return record;
+}; //end of function
+
+console.log('Adding an record to our collection', addToCollection('When All That\'s Left Is You', 'Quietdrive', '2006'));
+console.log(collection);
+addToCollection('SCIENCE', 'Incubus', '1998')
+console.log(collection);
