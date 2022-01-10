df8133e028a5e995b09a4b04dc51a34a1161a7a0
Brandon Lanier <brandonjlanier@gmail.com>
2022-01-10 12:38:55 -0600

    Added 6 albums and logged each to make sure return of function was working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,16 +3,19 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-  let record = {
-    title,
-    artist,
-    yearPublished
-  } // end of object
-  collection.push(record);
-  return record;
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }; // end of object
+  collection.push(album);
+  return album;
 }; //end of function
 
-console.log('Adding an record to our collection', addToCollection('When All That\'s Left Is You', 'Quietdrive', '2006'));
-console.log(collection);
-addToCollection('SCIENCE', 'Incubus', '1998')
-console.log(collection);
+console.log('Adding an record to our collection', addToCollection('When All That\'s Left Is You', 'Quietdrive', 2006));
+// return is working per console in browser.
+console.log('Adding album to our collection', addToCollection('SCIENCE', 'Incubus', 1997));
+console.log('Adding another album from an already used artist', addToCollection('Deliverance', 'Quietdrive', 2008));
+console.log('Adding a new album', addToCollection('Futuers', 'Jimmy Eat World', 2001));
+console.log('Adding another great album', addToCollection('Love is Dead', 'Chvrches', 2018));
+console.log('Adding last album', addToCollection('Rumors', 'Fleetwood Mac', 1977));
