2bcbed46133c88b37e290dda887317938eecf765
Brandon Lanier <brandonjlanier@gmail.com>
2022-01-10 12:50:07 -0600

    Added the show collection function and tested it.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,7 +4,7 @@ let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
   let album = {
-    title: title,
+    title: title, //This also works by just putting title here and not title: title.  Wasn't sure the proper formatting
     artist: artist,
     yearPublished: yearPublished
   }; // end of object
@@ -19,3 +19,14 @@ console.log('Adding another album from an already used artist', addToCollection(
 console.log('Adding a new album', addToCollection('Futuers', 'Jimmy Eat World', 2001));
 console.log('Adding another great album', addToCollection('Love is Dead', 'Chvrches', 2018));
 console.log('Adding last album', addToCollection('Rumors', 'Fleetwood Mac', 1977));
+
+console.log(`Lets take a look at our music collection`, collection);
+
+
+function showCollection(array) {
+  console.log(array.length);
+  for (let record of array) {
+    console.log(record);
+  }
+}
+console.log('Testing our showCollection function', showCollection(collection));
