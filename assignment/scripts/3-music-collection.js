4fd8b991e8c242467b961ac69e5260c69ec9c17e
Brandon Lanier <brandonjlanier@gmail.com>
2022-01-15 13:47:50 -0600

    reverting back to my original tracks function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,13 +2,14 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+
 function addToCollection(title, artist, yearPublished, tracks) {
   let album = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
     tracks: tracks
-  } // end of object
+  }; // end of object
   collection.push(album);
   return album;
 } // end of function
@@ -17,7 +18,7 @@ console.log('Adding an record to our collection', addToCollection('When All That
 // return is working per console in browser.
 console.log('Adding album to our collection', addToCollection('SCIENCE', 'Incubus', 1997, tracks('Redefine', '3\:20', 'Vitamin', '3\:13', 'New Skin', '3\:51')));
 console.log('Adding another album from an already used artist', addToCollection('Deliverance', 'Quietdrive', 2008, tracks('Believe', '3\:31', 'Deliverance', '3\:22', 'Motivation', '2\:50')));
-console.log('Adding a new album', addToCollection('Futures', 'Jimmy Eat World', 2001, tracks('Futures', '3\:58', 'Just Tonight', '3\:26', 'Work', '3\:24')));
+console.log('Adding a new album', addToCollection('Futures', 'Jimmy Eat World', 2004, tracks('Futures', '3\:58', 'Just Tonight', '3\:26', 'Work', '3\:24')));
 console.log('Adding another great album', addToCollection('Love is Dead', 'Chvrches', 2018, tracks('Graffiti', '3\:39', 'Get Out', '3\:51', 'Deliverance', '4\:13')));
 console.log('Adding last album', addToCollection('A Brief Inquiry into Online Relationships', 'The 1975', 2018, tracks('The 1975', '1\:34', 'Give Yourself A Try', '3\:17', 'TooTimeTooTimeTooTime', '3\:28')));
 console.log(`Lets take a look at our music collection`, collection);
@@ -25,13 +26,13 @@ console.log(`Lets take a look at our music collection`, collection);
 
 function tracks(songTitle1, songDur1, songTitle2, songDur2, songTitle3, songDur3) {
   let tracks = [] // empty array to push the tracks to.  Assignment calls for tracks to be in an array.
-  let track1 = {
+  let track1 = { // Exploring different ways to enter tracks, but this works currently.
     name: songTitle1,
     duration: songDur1
   };
   let track2 = {
     name: songTitle2,
-    duration: songDur2,
+    duration: songDur2
   };
   let track3 = {
     name: songTitle3,
@@ -40,8 +41,6 @@ function tracks(songTitle1, songDur1, songTitle2, songDur2, songTitle3, songDur3
   tracks.push(track1, track2, track3); // pushing all track objects into array.
   return tracks;
 }
-
-
 
 
 function showCollection(array) {
@@ -57,8 +56,6 @@ function showCollection(array) {
 console.log(`Testing out our show collection function:`);
 showCollection(collection); // Check console for results.
 // If I call this function inside a console log it will run the function but also include an undefined statment at the end of running the function.
-
-
 
 function findByArtist(artists) {
   let results = [];
@@ -89,11 +86,11 @@ function search(artist, year, trackName) {
       }
     }
   }
-  return newResults;
+  return newResults; //if not match found return empty array. Otherwise return album.
 }
 
 
 console.log('Testing with an album that is not in the collection (Should be empty array):', search('Ray Chalres', 1997, 'Song'));
 console.log('Testing if no arguments are entered into the search function (Should return the collection):', search());
 console.log('Testing with track name (Should return 1 album):', search('The 1975', 2018, 'The 1975'));
-console.log('Testing to see if our search works with all arguments being found (Should return 1 album):', search('Quietdrive', 2006, 'Get Up'));
+console.log(`Testing to see if our search works with all arguments being found (Should return 1 album)`, search('Quietdrive', 2006, 'Get Up'));
